2ccd5cdab6cbe94689e7d3f52cd0fb81d8196361
satan <e-mezrin2012@yandex.ru>
2023-01-30 21:04:08 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String[] dirNamesGames = new String[] {"src", "res", "savegames", "temp"};
+        String[] dirNamesSrc = new String[] {"main", "test"};
+        String[] fileNamesMain = new String[] {"Main.java", "Utils.java"};
+        String[] dirNamesRes = new String[] {"drawables", "vectors", "icons"};
+        StringBuilder logs = new StringBuilder();
+
+
+        for(String name: dirNamesGames){
+            File dir = new File("D://Games/" + name);
+            dir.mkdir();
+            logs.append("Корневая папка " + name + " создана\n");
+        }
+
+        for(String name: dirNamesSrc){
+            File dir = new File("D://Games/src/" + name);
+            dir.mkdir();
+            logs.append("Папка в каталоге src " + name + " создана\n");
+        }
+
+        for(String name: fileNamesMain){
+            File file = new File("D://Games/main" + name);
+
+            if(file.createNewFile()){
+                logs.append("Файл " + name + " создан\n");
+            }
+            else logs.append("Файл " + name + " не создан\n");
+
+        }
+
+        for(String name: dirNamesRes){
+            File dir = new File("D://Games/res/" + name);
+            dir.mkdir();
+            logs.append("Папка в каталоге res " + name + " создана\n");
+        }
+
+        File temp = new File("D://Games/temp",  "temp.txt");
+
+        if(temp.createNewFile()){
+            logs.append("Файл temp создан\n");
+        }
+        else logs.append("Файл temp не создан\n");
+
+        FileWriter fw = new FileWriter(temp);
+        fw.write(logs.toString());
+
+        System.out.println(logs);
+
+
+
+    }
+}
